957f0d26fa1c776f008cffbf0cb03225324db033
Tzheng456 <Tzheng456@gmail.com>
2021-10-23 02:43:09 -0700

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <h1>HELLO</h1>
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav(props) {
+  return(
+    <div className="navbar-container">
+      <ul id="menu-primary-menu">
+        <li>Home</li>
+        <li>Projects</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+}
