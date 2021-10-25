2201b50f77e14a702547049a451bec5a8e167ef6
Tzheng456 <Tzheng456@gmail.com>
2021-10-25 14:47:31 -0700

    fix fontawesome link and footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,17 @@ import "./Footer.scss";
 export default function Footer(props) {
   return (
     <div className="footer-container">
-      <ul className="social-links-container">
-        <li className="social-link-item">
-          
-          <a className="link-item" href="http://github.com/Tzheng456"><i className="fa-brands fa-github-square"></i>GitHub</a>
-        </li>
-        <li className="social-link-item">
-          <i className="fa-brands fa-linkedin"></i>
-          <a className="link-item" href="https://www.linkedin.com/in/tim-zheng-084510217/">LinkedIn</a>
-        </li>
-        <li className="social-link-item">
-          <i className="fa-solid fa-square-envelope"></i>
-          <a className="link-item" href="mailto:Tzheng456@gmail.com">Contact Me</a>
-        </li>
-      </ul>
+      <div className="social-links-container">
+        <div className="social-link-item">  
+          <a className="link-item" href="http://github.com/Tzheng456"><i className="fab fa-github-square"></i> GitHub</a>
+        </div>
+        <div className="social-link-item">
+          <a className="link-item" href="https://www.linkedin.com/in/tim-zheng-084510217/"><i className="fab fa-linkedin"></i> LinkedIn</a>
+        </div>
+        <div className="social-link-item">
+          <a className="link-item" href="mailto:Tzheng456@gmail.com"><i className="fas fa-envelope-square"></i> Contact Me</a>
+        </div>
+      </div>
       <div className="copyright-msg">
         <p>
           © 2021 <a href="http://github.com/Tzheng456">Tim Zheng</a>. All rights reserved.
