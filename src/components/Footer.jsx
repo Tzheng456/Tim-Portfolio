d544978faa674322f6a3009f93d918c19e06dc5c
Tzheng456 <Tzheng456@gmail.com>
2021-10-25 16:38:40 -0700

    add more styling to footer and nav, put social links in nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,16 @@ export default function Footer(props) {
     <div className="footer-container">
       <div className="social-links-container">
         <div className="social-link-item">  
-          <a className="link-item" href="http://github.com/Tzheng456"><i className="fab fa-github-square"></i> GitHub</a>
+          <a className="link-item" href="http://github.com/Tzheng456"><i className="fab fa-github"></i></a>
         </div>
         <div className="social-link-item">
-          <a className="link-item" href="https://www.linkedin.com/in/tim-zheng-084510217/"><i className="fab fa-linkedin"></i> LinkedIn</a>
+          <a className="link-item" href="https://www.linkedin.com/in/tim-zheng-084510217/"><i className="fab fa-linkedin"></i></a>
         </div>
         <div className="social-link-item">
-          <a className="link-item" href="mailto:Tzheng456@gmail.com"><i className="fas fa-envelope-square"></i> Contact Me</a>
+          <a className="link-item" href="https://www.facebook.com/tim.zheng.37/"><i className="fab fa-facebook"></i></a>
+        </div>
+        <div className="social-link-item">
+          <a className="link-item" href="mailto:Tzheng456@gmail.com"><i className="fas fa-envelope"></i></a>
         </div>
       </div>
       <div className="copyright-msg">
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,9 +7,17 @@ export default function Nav(props) {
     <Fragment>
       <div className="navbar-container">
         <div id="menu-primary-menu">
-          <Link to="/" className="menu-item">Home</Link>
-          <Link to="/projects" className="menu-item">Projects</Link>
-          <Link to="/about" className="menu-item">About</Link>
+          <div className="navbar-pages">
+            <Link to="/" className="menu-item">Home</Link>
+            <Link to="/projects" className="menu-item">Projects</Link>
+          </div>
+          <div className="navbar-socials">
+            <a className="menu-item" href="http://github.com/Tzheng456"><i className="fab fa-github fa-lg"></i></a>
+            <a className="menu-item" href="https://www.linkedin.com/in/tim-zheng-084510217/"><i className="fab fa-linkedin fa-lg"></i></a>
+            <a className="menu-item" href="https://www.facebook.com/tim.zheng.37/"><i className="fab fa-facebook fa-lg"></i></a>
+            <a className="menu-item" href="mailto:Tzheng456@gmail.com"><i className="fas fa-envelope fa-lg"></i></a>
+          </div>
+          {/* <Link to="/about" className="menu-item">About</Link> */}
         </div>
       </div>
       <div id="nav-bar-spacer"></div>
