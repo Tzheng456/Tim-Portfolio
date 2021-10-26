71e6ac25ccea55c6854e7f87ba5e1518ad19d5ba
Tzheng456 <Tzheng456@gmail.com>
2021-10-25 17:57:01 -0700

    add some body text to home, change styling of home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,6 +13,18 @@ export default function Home(props) {
         <h1>Tim Zheng</h1>
         <h3>Full-Stack Web Developer</h3>
       </div>
+      <div className="home-body-text intro">
+        <p>
+          Welcome to my portfolio.<br/><br/>
+          I am a Full-Stack Web Developer who earned a diploma from Lighthouse Labs. I transitioned to programming from an Arts degree at the University of British Columbia in Vancouver, BC, Canada.
+        </p>
+      </div>
+      <div className="home-body-text skills">
+        <h1>Skillset</h1>
+        <p>
+        Languages I've worked with:
+        </p>
+      </div>
     </div>
   );
 }
